9da397cca46d351cd16d05cea6e4ebbe9b1899bc
Paulmichael Contreras <git@pmcizhere.com>
2017-02-27 21:07:18 -0800

    Initial version, jQuery stuff.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<html>
+<head>
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="js/jquery-ui.css" />
+<title>Presidential Bingo</title>
+</head>
+<body>
+<div class="picker">
+	<div class="square grid-elm-3">I am a quote.</div>
+</div>
+<div class="board">
+	<div class="square grid-elm-3">I am a quote.</div>
+</div>
+</body>
+</html>
